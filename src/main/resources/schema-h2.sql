4c8d13ba3850233c2b9a42c921744fc2cda4bd2b
glenn <glenn.jo@kakaocorp.com>
2020-11-11 17:35:41 +0900

    add sample user insert

---
+++ src/main/resources/schema-h2.sql
@@ -0,0 +1,9 @@
+create table user(
+    id INT NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+);
+
+insert into user values (1, 'test1', 10);
+insert into user values (2, 'test2', 20);
+insert into user values (3, 'test3', 30);
--- src/test/java/com/example/r2dbc/R2dbcApplicationTests.java
+++ src/test/java/com/example/r2dbc/R2dbcApplicationTests.java
@@ -2,31 +2,38 @@ package com.example.r2dbc;
 
 import com.example.r2dbc.repository.UserRepository;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import reactor.test.StepVerifier;
 
+//@ActiveProfiles("h2")
 @SpringBootTest
 class R2dbcApplicationTests {
 
 	@Autowired
 	private UserRepository userRepository;
 
+	@DisplayName("사용자 테이블 deleteAll 테스트")
     @Test
-    public void whenDeleteAll_then0IsExpected() {
+    public void whenDeleteAllThen0IsExpected() {
         userRepository.deleteAll()
                 .as(StepVerifier::create)
                 .expectNextCount(0)
                 .verifyComplete();
     }
 
+    @DisplayName("사용자 테이블 findAll 테스트")
     @Test
-    public void whenInsert6_then6AreExpected() {
+    public void whenFindAllThen3AreExpected() {
         userRepository.findAll()
                 .as(StepVerifier::create)
-                .expectNextCount(6)
+                .expectNextCount(3)
                 .verifyComplete();
     }
 
