e6a17c35268a707d4cf8fdb420842058d2950103
glenn <glenn.jo@kakaocorp.com>
2020-11-15 21:25:53 +0900

    add test case

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,9 +1,9 @@
-create table user(
-    id INT NOT NULL,
+create table user (
+    id BIGINT AUTO_INCREMENT NOT NULL,
     name VARCHAR(50) NOT NULL,
     age INT NOT NULL
 );
 
-insert into user values (1, 'test1', 10);
-insert into user values (2, 'test2', 20);
-insert into user values (3, 'test3', 30);
+insert into user (name, age) values ('test1', 10);
+insert into user (name, age) values ('test2', 20);
+insert into user (name, age) values ('test3', 30);
