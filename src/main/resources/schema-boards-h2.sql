bc354650481eb7056e447feb12d9f2c7eb3be9d2
glenn <glenn.jo@kakaocorp.com>
2020-12-02 22:31:24 +0900

    spring 버전 업

--- src/main/resources/schema-boards-h2.sql
+++ src/main/resources/schema-boards-h2.sql
@@ -7,5 +7,5 @@ create table boards (
 );
 
 insert into boards (title, content, updated_at, created_at) values ('Title 1', 'Content1', now(), now());
-insert into boards (title, content, updated_at, created_at) values ('Title 1', 'Content1', now(), now());
-insert into boards (title, content, updated_at, created_at) values ('Title 1', 'Content1', now(), now());
+insert into boards (title, content, updated_at, created_at) values ('Title 2', 'Content2', now(), now());
+insert into boards (title, content, updated_at, created_at) values ('Title 3', 'Content3', now(), now());
